656fa36f06634400a319ff699cb0706d446e34b3
TFMV <mcgeehan@gmail.com>
2024-05-28 15:17:30 -0500

    docs

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -5,5 +5,7 @@ export SCRIPT_PATH=/Users/thomasmcgeehan/AddressMatchPro/AddressMatchPro/python-
 
 python -m spacy download en_core_web_md
 
+pip install mkdocs mkdocs-material ghp-import
+
 go run /Users/thomasmcgeehan/AddressMatchPro/AddressMatchPro/cmd/addressmatchpro/main.go
 
